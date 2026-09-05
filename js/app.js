b2202fe8035b90d20c0efe8942019cd9c64b1f71
ElevationCreekStudio <49608231+ElevationCreekStudio@users.noreply.github.com>
2026-09-05 04:39:49 +0300

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Проверяем, поддерживает ли браузер Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('../sw.js')
+      .then((reg) => {
+        console.log('Service Worker успешно зарегистрирован! Сфера действия:', reg.scope);
+      })
+      .catch((error) => {
+        console.log('Ошибка при регистрации Service Worker:', error);
+      });
+  });
+}
