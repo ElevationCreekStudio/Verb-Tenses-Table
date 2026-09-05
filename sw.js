6ec0525e82c8eae5258ed704ac174c62988dad2f
ElevationCreekStudio <49608231+ElevationCreekStudio@users.noreply.github.com>
2026-09-05 04:36:45 +0300

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'verb-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/main.css',
+  './js/main.js',
+  './js/renders.js',
+  './verbs/verbs_irregular.json',
+  './verbs/verbs_regular.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Событие установки воркера
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Событие активации
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker активирован');
+});
+
+// Перехват запросов (работа в офлайне)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+                      
